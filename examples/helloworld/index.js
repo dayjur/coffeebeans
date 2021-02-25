04f46f6bc950e6385c8dba71e08a301c17881237
dayjur <richf2000@outlook.com>
2021-02-25 17:47:23 -0500

    added example folder and hello world

---
+++ examples/helloworld/index.js
@@ -0,0 +1,11 @@
+const app=require('../..'); // for testing from this dir normally require('coffeebeans');
+
+var routes=app.router; // get the router
+
+// semd 'Hello World' to browser localhost:3000/ 
+routes.get('/', (req, res) => {
+    app.response.sendText(res,'Hello World!');
+    });
+
+
+app.listen(process.env.PORT || 3000); // listen on platform assigned port or localhost:3000
