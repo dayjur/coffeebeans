1ff9facc865da89318db67195695785108f08210
dayjur <richf2000@outlook.com>
2021-02-26 15:00:57 -0500

    allowed new instances of routes

--- examples/helloworld/index.js
+++ examples/helloworld/index.js
@@ -1,11 +1,13 @@
-const app=require('../..'); // for testing from this dir normally require('coffeebeans');
+const app = require('../..'); // for testing from this dir normally require('coffeebeans');
 
-var routes=app.router; // get the router
+var routes = app.router(); // get a router
 
-// semd 'Hello World' to browser localhost:3000/ 
+// set routes
 routes.get('/', (req, res) => {
-    app.sendText('Hello World!');
-    });
+    app.sendText('Hello World!');// semd 'Hello World' to browser localhost:3000/ 
+});
 
+// use the routes
+app.use(routes);
 
 app.listen(process.env.PORT || 3000); // listen on platform assigned port or localhost:3000
